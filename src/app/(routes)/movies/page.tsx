0cd21d756d5f3a0a300b7c6b579a48e40fc4e598
AndriySysoenko <gaskonets@gmail.com>
2024-11-18 19:25:53 +0200

    create pagination, added libraries

--- src/app/(routes)/movies/page.tsx
+++ src/app/(routes)/movies/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import MoviesList from "@/app/components/movies/MoviesList";
 import {getMovies} from "@/app/services/api.service";
+import MoviesPagination from "@/app/components/pagination/MoviesPagination";
 
 const MoviesPage = async () => {
     const movies = await getMovies();
@@ -8,6 +9,7 @@ const MoviesPage = async () => {
     return (
         <div>
             <MoviesList movies={movies}/>
+            {/*<MoviesPagination/>*/}
         </div>
     );
 };
--- src/app/services/api.service.tsx
+++ src/app/services/api.service.tsx
@@ -1,4 +1,5 @@
 import {IMovie} from "@/app/models/IMovie";
+import exp from "node:constants";
 
 type MovieDBResponse = {
     page: number,
@@ -6,21 +7,41 @@ type MovieDBResponse = {
     total_pages: number,
     total_results: number
 }
-export const getMovies= async (): Promise<IMovie[]> => {
-     const responseMovieDB: MovieDBResponse = await fetch('https://api.themoviedb.org/3/discover/movie?page=1', {
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U'
-        }
-    })
-    if (!responseMovieDB.ok) {
-        throw new Error(`HTTP error! status: ${responseMovieDB.status}`);
-    }
+// export const getMovies= async (): Promise<IMovie[]> => {
+//      const responseMovieDB: MovieDBResponse = await fetch('https://api.themoviedb.org/3/discover/movie?page=1', {
+//         headers: {
+//             accept: 'application/json',
+//             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U'
+//         }
+//     })
+//     if (!responseMovieDB.ok) {
+//         throw new Error(`HTTP error! status: ${responseMovieDB.status}`);
+//     }
+//
+//     const data = await responseMovieDB.json();
+//     return data.results;
+// }
 
-    const data = await responseMovieDB.json();
-    // console.log(data);
-    return data.results;
-}
+export const getMovies = async (currentPage: number): Promise<MovieDBResponse> => {
+    try {
+        const responseMovieDB: MovieDBResponse = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${currentPage}`, {
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U`,
+            },
+        });
+
+        if (!responseMovieDB.ok) throw new Error('Ошибка загрузки данных');
+
+        return await responseMovieDB.json();
+
+    } catch (error) {
+        console.error('Ошибка:', error);
+    }
+};
+
+
+
 
 export const getMovie= async (id:string): Promise<IMovie> => {
     const movieDetails = await fetch('https://api.themoviedb.org/3/movie' + '/' + id, {
