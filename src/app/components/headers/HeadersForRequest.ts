efd5ae137034f667fec2c078207765d6d92d8add
AndriySysoenko <gaskonets@gmail.com>
2024-11-20 16:24:34 +0200

    refactoring api.service.tsx

---
+++ src/app/components/headers/HeadersForRequest.ts
@@ -0,0 +1,6 @@
+export const HeadersForRequest = {
+headers: {
+        accept: "application/json",
+        Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U`,
+        }
+}
--- src/app/services/api.service.tsx
+++ src/app/services/api.service.tsx
@@ -1,98 +1,31 @@
-import {IGenre, IKeyword, IMovie} from "@/app/models/IMovie";
-
-type MovieDBResponse = {
-    page: number,
-    results:IMovie[],
-    total_pages: number,
-    total_results: number
-}
+import {IGenre, IMovie} from "@/app/models/IMovie";
+import {MovieDBResponse} from "../models/MovieDBResponse";
+import {HeadersForRequest} from "../components/headers/HeadersForRequest";
 
 export const getMovies = async (page: number): Promise<MovieDBResponse> => {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}`, {
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U`,
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch movies");
-    }
-
-    const data: MovieDBResponse = await response.json();
-    return data;
+    return await fetch(`https://api.themoviedb.org/3/discover/movie?page=${page}`, HeadersForRequest)
+        .then(value => value.json());
 }
 
 export const getMovie= async (id:string, name:string): Promise<IMovie> => {
-    const movieDetails = await fetch(`https://api.themoviedb.org/3/movie/${id}${name}`, {
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U'
-        }
-    })
-    // if (!movieDetails.ok) {
-    //     throw new Error(`HTTP error! status: ${movieDetails.status}`);
-    // }
-
-    const data = await movieDetails.json();
-    return data
+    return await fetch(`https://api.themoviedb.org/3/movie/${id}${name}`, HeadersForRequest)
+        .then(value => value.json());
 }
 
 type ResponseGenre = {
     gеnres: IGenre[]
 }
 export const getGenres = async (): Promise<ResponseGenre> => {
-    const responseGenre:ResponseGenre = await fetch('https://api.themoviedb.org/3/genre/movie/list', {
-        headers: {
-            accept: "application/json",
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U',
-        }
-    }).then(value => value.json())
-    
-    return responseGenre;
+    return await fetch('https://api.themoviedb.org/3/genre/movie/list', HeadersForRequest)
+        .then(value => value.json());
 }
 
 export const getMoviesByGenre = async (genreId: number, page: number): Promise<MovieDBResponse> => {
-    console.log(genreId, page)
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}&page=${page}`, {
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U`,
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch movies by genre");
-    }
-
-    const data:MovieDBResponse = await response.json();
-    console.log(data)
-    return data
+    return await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreId}&page=${page}`, HeadersForRequest)
+        .then(value => value.json());
 };
-
-type ResponseKeywords = {
-    page: number;
-    results: IKeyword[];
-    total_pages: number;
-    total_results: number;
-}
 
 export const searchMovieByKeyword = async (query: string, page: number): Promise<MovieDBResponse> => {
-    console.log(query, page)
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&page=${page}`, {
-        headers: {
-            accept: "application/json",
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzOGRiOTEwNDY3ZTAwZWU0Y2FmZWZkZDQzMjhlOGEwNyIsIm5iZiI6MTczMTkxNTcyNy45MTg2MTk5LCJzdWIiOiI2NzM2ZjBiNTZiZDQ4ODliZmJjNzlkY2QiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.TtEHl9DPyW0vPB4WYcuZllBnLqp6xpQqxj1KrIGJr5U`,
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error("Failed to fetch movies by genre");
-    }
-
-    const data:MovieDBResponse = await response.json();
-    console.log(data)
-    return data
+    return await fetch(`https://api.themoviedb.org/3/search/movie?query=${encodeURIComponent(query)}&page=${page}`, HeadersForRequest)
+        .then(value => value.json())
 };
-
-// https://api.themoviedb.org/3/search/movie?query=
